d8f341fd9903feb2cb4f87e99ad0a6f456a0c71a
aizadrzo <aizadridzoo@gmail.com>
2021-02-23 21:28:28 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const pokeContainer = document.getElementById("poke-container");
+const pokeNum = 150;
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+const main_types = Object.keys(colors);
+
+//Looping through the pokemon
+const pokeFetch = async () => {
+  for (let i = 1; i <= pokeNum; i++) {
+    await pokeGet(i);
+  }
+};
+
+//Sending GET request to the server
+const pokeGet = async (id) => {
+  apiUrl = "https://pokeapi.co/api/v2/pokemon/" + id;
+  await axios.get(apiUrl).then((res) => {
+    const pokemon = res.data;
+    createPokeCard(pokemon);
+  });
+};
+
+//Creating Pokemon Cards to display img, name and type
+const createPokeCard = (pokemon) => {
+  const pokeElement = document.createElement("div");
+  pokeElement.classList.add("pokemon");
+
+  const pokeType = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => pokeType.indexOf(type) > -1);
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const color = colors[type];
+
+  pokeElement.style.backgroundColor = color;
+
+  pokeElement.innerHTML = `
+  <div class = 'img-container'>
+  <img src='https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png'></img>
+  </div>
+  <div class='description-container'>
+  <p class='p-id'>#${pokemon.id}</p>
+  <h4 class='p-name'>${name}</h4>
+  <p class='p-type'>${type}</p>
+  </div>
+    `;
+
+  pokeContainer.appendChild(pokeElement);
+};
+
+pokeFetch();
